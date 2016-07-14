2b4d7b119c5b0c66a5fa1bd8e50d3c8a43f838d3
iralsob <iralsob@gmail.com>
2016-07-14 18:46:09 +0300

    змейка толстеет на килограмм

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-var rabbit, snake, area;
+var rabbit, snake, area, length = 0;
 
+function Snake (meal) {
 
-
-function Snake (snakeId, meal) {
-
-	this.html = document.getElementById(snakeId);
-
-	this.width = 10;
-	this.height = 10;
-	this.html.style.width = this.width+'px';
-	this.html.style.height = this.height+'px';
 
 	this.init = function() {
-		this.left = 0;
-		this.top = 0;
-		this.directionRight = 0;
-		this.directionTop = 1;
+		element = document.getElementById('gameArea')
+				.appendChild(document.createElement('div'));
+		element.id = 'element-'+length;
+		element.className = 'snake';
+
+		this.html = document.getElementById('element-'+length); //создаем html под блок змейки
+		this.width = 10;
+		this.height = 10;
+		this.html.style.width = this.width+'px';
+		this.html.style.height = this.height+'px';
+
+
+		if (length == 0) {
+			this.left = 0;
+			this.top = 0;
+			this.directionRight = 0;
+			this.directionTop = 1;
+		} else {
+			this.directionRight = prevRight;
+			this.directionTop = prevTop;
+		}
+		
+		prevRight = this.directionRight;
+		prevTop = this.directionTop;
 
 		this.html.style.left = this.left+'px';
 		this.html.style.top = this.top+'px';
@@ -30,52 +42,43 @@ function Snake (snakeId, meal) {
 			this.top += 10 * this.directionTop;
 		} else {
 			console.log('stop game');
+			length = 0;
 			this.init();
 		}
 		this.eat(meal);
 	}
 
 	this.turn = function(e) {
-		console.log('turn');
 		e = e || window.event;
   		if (e.keyCode === 39) {
-  			console.log('turn');
-			if (this.directionTop == 1) {
+  			if (this.directionTop != 0) {
+				this.directionRight = - this.directionTop;
 				this.directionTop = 0;
-				this.directionRight = -1;
-			} else if (this.directionTop == -1) {
-				this.directionTop = 0;
-				this.directionRight = 1;
-			} else if (this.directionRight == 1) {
+			} else {
+				this.directionTop = this.directionRight;
 				this.directionRight = 0;
-				this.directionTop = 1;
-			} else if (this.directionRight == -1) {
-				this.directionRight = 0;
-				this.directionTop = -1;
 			}
 		} else if (e.keyCode === 37) {
-			console.log('turn');
-		    if (this.directionTop == 1) {
+		    if (this.directionTop != 0) {
+				this.directionRight = this.directionTop;
 				this.directionTop = 0;
-				this.directionRight = 1;
-			} else if (this.directionTop == -1) {
-				this.directionTop = 0;
-				this.directionRight = -1;
-			} else if (this.directionRight == 1) {
+			} else {
+				this.directionTop = - this.directionRight;
 				this.directionRight = 0;
-				this.directionTop = -1;
-			} else if (this.directionRight == -1) {
-				this.directionRight = 0;
-				this.directionTop = 1;
 			}
 	  	}
-	  
+	  	
+	  	prevRight = this.directionRight;
+		prevTop = this.directionTop;
+
 	  	return false;
 	}
 
 	this.eat = function(rabbit) {
 		if(this.left == rabbit.left && this.top == rabbit.top) {
 	    	alert("Eat!");
+	    	length = length + 1;
+	    	this.init();
 	    	rabbit.show();
 	    }
 	}
@@ -114,5 +117,5 @@ window.onload = function() {
 
     
     rabbit = new Rabbit('rabbit');
-    snake = new Snake('snake', rabbit);
+    snake = new Snake(rabbit);
 }
